f9523a7ad696084bb30de9d2fc30108653e8284a
eduardo.montoya <eduardo.montoya@feek.app>
2022-08-18 08:46:12 -0500

    refactor: comment category_name

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ module.exports = (username) => {
 			const avatar = res.body.split('"profile_pic_url":"')[1].split('",')[0];
 			const instagram_id = res.body.split(',"id":"')[1].split('",')[0];
 			const full_name = res.body.split('"full_name":"')[1].split('",')[0];
-			const category_name = res.body
-				.split('"category_name":"')[1]
-				.split('",')[0];
+			// const category_name = res.body
+			// 	.split('"category_name":"')[1]
+			// 	.split('",')[0];
 			const username = res.body.split(',"username":"')[1].split('",')[0];
 			const is_professional_account = res.body
 				.split(',"is_professional_account":')[1]
@@ -29,7 +29,7 @@ module.exports = (username) => {
 				avatar: avatar,
 				instagram_id: instagram_id,
 				full_name: full_name,
-				category_name: category_name,
+				// category_name: category_name,
 				username: username,
 				is_professional_account: is_professional_account,
 			};
